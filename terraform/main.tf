0b78df523c9f0bff2c3d0c1928b96fb80de87e21
Sooraj <soorajskr@gmail.com>
2026-06-22 13:44:26 +0530

    backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,13 @@ terraform {
     }
   }
   required_version = ">= 1.3.0"
+
+  backend "azurerm" {
+    resource_group_name  = "terraform-state-rg"
+    storage_account_name = "tfdevbackendsooraj"
+    container_name       = "tfstate"
+    key                  = "ghe-copilot-miniproject.tfstate"
+  }
 }
 
 provider "azurerm" {
